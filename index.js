2594557bae1c215b1335f9e78f1f707ae10bd1ed
Whales D'bayor <wabalonline01@gmail.com>
2025-02-01 16:49:13 +0100

    New Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// import express from "express";
+
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable CORS handling
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    email: "wabalonline01@gmail.com",
+    current_date: new Date().toISOString(),
+    github_acct: "https://github.com/WhalesDbayor/public-api",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
